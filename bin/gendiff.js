930745845e73f85c9c447cba39c3cfe7e0b40449
Annwin <djandreev89@mail.ru>
2021-04-16 15:41:02 +1000

    add commander

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
-console.log('test');
+import { Command } from 'commander/esm.mjs';
+const program = new Command();
+
+program
+  //.option('-h, --help', 'output usage information')
+  .option('-V, --version', 'output the version number');
+
+program.addHelpText('before', 
+`
+Compares two configuration files and shows a difference.`);
+
+
+program.parse(process.argv);
+
+// const options = program.opts();
+// if (options.version) console.log(options);
