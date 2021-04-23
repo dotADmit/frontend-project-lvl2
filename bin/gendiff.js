b8deb4a2f1555c7e0d7fa4b021de4380e2bbebdf
Annwin <djandreev89@mail.ru>
2021-04-23 12:59:18 +1000

    update gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
+import fs from 'fs';
+import diff from '../src/index.js';
 
 const program = new Command();
 
@@ -9,6 +11,12 @@ program
   .version('1.0.0', '-V, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .action((path1, path2) => {
+    const a = fs.readFileSync(path1, 'utf8');
+    const b = fs.readFileSync(path2, 'utf8');
+
+    console.log(diff(JSON.parse(a), JSON.parse(b)));
+  });
 
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import _ from 'lodash';
 
-const a1 = {
-  "host": "hexlet.io",
-  "timeout": 50,
-  "proxy": "123.234.53.22",
-  "follow": false
-};
-
-const b1 = {
-  "timeout": 20,
-  "verbose": true,
-  "host": "hexlet.io"
-};
-
-const diff = (obj1, obj2) => {
+export default (obj1, obj2) => {
   const merged = _.merge({}, obj1, obj2);
 
   const props = Object.entries(merged).flatMap(([key, value]) => {
@@ -27,16 +14,14 @@ const diff = (obj1, obj2) => {
     return _.has(obj1, key) ? changedInFirst : changedInSecond;
   });
 
-  const sortedProps = props.sort((a, b) => {
+  props.sort((a, b) => {
     const keyA = a.slice(2, a.indexOf(':'));
     const keyB = b.slice(2, b.indexOf(':'));
     if (keyA > keyB) return 1;
     if (keyA < keyB) return -1;
     return a > b ? -1 : 1;
   });
-  const propsToString = sortedProps.reduce((acc, prop) => `${acc}${prop}\n`, '');
 
+  const propsToString = props.reduce((acc, prop) => `${acc}${prop}\n`, '');
   return `{\n${propsToString}}`;
 };
-
-console.log(diff(a1, b1));
