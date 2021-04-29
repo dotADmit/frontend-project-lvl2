735c1d980afcba2e17182b4fc0f30bd9ad3b1c47
Annwin <djandreev89@mail.ru>
2021-04-29 13:23:11 +1000

    add new fuxtures, update gendiff.test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,6 +11,51 @@ const result = `{
 + verbose: true
 }`;
 
+const result2 = `{
+  common: {
+    + follow: false
+      setting1: Value 1
+    - setting2: 200
+    - setting3: true
+    + setting3: null
+    + setting4: blah blah
+    + setting5: {
+          key5: value5
+      }
+      setting6: {
+          doge: {
+            - wow: 
+            + wow: so much
+          }
+          key: value
+        + ops: vops
+      }
+  }
+  group1: {
+    - baz: bas
+    + baz: bars
+      foo: bar
+    - nest: {
+          key: value
+      }
+    + nest: str
+  }
+- group2: {
+      abc: 12345
+      deep: {
+          id: 45
+      }
+  }
++ group3: {
+      deep: {
+          id: {
+              number: 45
+          }
+      }
+      fee: 100500
+  }
+}`;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -26,4 +71,10 @@ test('common work yml', () => {
   const yml1 = getFixturePath('file1.yaml');
   const yml2 = getFixturePath('file2.yml');
   expect(diff(yml1, yml2)).toEqual(result);
+});
+
+test('recursive work json', () => {
+  const json1 = getFixturePath('file3.json');
+  const json2 = getFixturePath('file4.json');
+  expect(diff(json1, json2)).toEqual(result2);
 });
