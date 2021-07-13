e31ef252553d62736815efaa1a578f6fd666c123
Annwin <djandreev89@mail.ru>
2021-07-14 09:14:53 +1000

    refactoring

--- src/buildAST.js
+++ src/buildAST.js
@@ -7,9 +7,7 @@ const buildAST = (obj1, obj2) => {
   const ast = sortedUniqKeys.map((key) => {
     const [value1, value2] = [obj1[key], obj2[key]];
 
-    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return { type: 'nested', key, children: buildAST(value1, value2) };
-    }
+    if (_.isPlainObject(value1) && _.isPlainObject(value2)) return { type: 'nested', key, children: buildAST(value1, value2) };
     if (!_.has(obj1, key)) return { type: 'added', key, value: value2 };
     if (!_.has(obj2, key)) return { type: 'removed', key, value: value1 };
     if (_.isEqual(value1, value2)) return { type: 'unchanged', key, value: value1 };
